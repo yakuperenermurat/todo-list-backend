c1ae032110772df25d742fe5db7b9ce928e7fc6f
yakuperenermurat <yakuperenermurat@gmail.com>
2025-01-09 20:55:58 +0300

    entity oluşturdum

---
+++ src/main/java/com/yakuperenermurat/todolist/entity/Todo.java
@@ -0,0 +1,29 @@
+package com.yakuperenermurat.todolist.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Task name cannot be empty")
+    private String title;
+
+    private boolean completed;
+}
